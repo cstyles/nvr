0e3977212d1b3e720c058676d2c4176b67a723f9
Collin Styles <collingstyles@gmail.com>
2021-07-15 02:13:46 -0700

    Switch to std::env::var_os

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,24 @@
 use neovim_lib::{CallError, Neovim, NeovimApi, Session, Value};
 use std::env;
+use std::path::Path;
 use std::process::{exit, Command};
 use std::sync::mpsc::Receiver;
 
 fn main() {
     let arg = env::args().nth(1);
 
-    match env::var("NVIM_LISTEN_ADDRESS") {
-        Ok(listen_address) => open_in_existing_neovim(listen_address, arg).unwrap(),
-        Err(_) => launch_new_neovim_process(arg),
+    match env::var_os("NVIM_LISTEN_ADDRESS") {
+        Some(listen_address) => open_in_existing_neovim(listen_address, arg).unwrap(),
+        None => launch_new_neovim_process(arg),
     };
 }
 
-fn open_in_existing_neovim(listen_address: String, arg: Option<String>) -> Result<(), CallError> {
-    let (mut nvim, receiver) = connect_to_nvim(&listen_address);
+fn open_in_existing_neovim<T: AsRef<Path>>(
+    listen_address: T,
+    arg: Option<String>,
+) -> Result<(), CallError> {
+    let listen_address = listen_address.as_ref();
+    let (mut nvim, receiver) = connect_to_nvim(listen_address);
     nvim.command("split")?;
 
     let result = match arg {
@@ -84,7 +89,8 @@ fn set_up_augroup(nvim: &mut Neovim, channel_id: u64) -> Result<(), CallError> {
     nvim.command(&command)
 }
 
-fn connect_to_nvim(address: &str) -> (Neovim, Receiver<(String, Vec<Value>)>) {
+fn connect_to_nvim<T: AsRef<Path>>(address: T) -> (Neovim, Receiver<(String, Vec<Value>)>) {
+    let address = address.as_ref();
     let mut session = Session::new_unix_socket(address).unwrap();
 
     // Store a Receiver that we can use to read responses back from neovim
